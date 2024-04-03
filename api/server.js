dd511327e10d2d6eae85aea0181ce58cad2afd16
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-03 13:13:13 -0500

    wip routers working!

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const express = require('express')
+const authRouter = require('./auth/auth-router.js')
 
 const usersRouter = require('./users/users-router.js')
+
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../client')))
 server.use(express.json())
+server.use('/api/auth', authRouter)
 
 server.use('/api/users', usersRouter)
 
