eb062b0e00ec1297a5698d09618d40f9f606fe5f
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-03 14:06:44 -0500

    wip auth middle

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,6 +4,7 @@ const User = require('../users/users-model')
 
 
 const router = express.Router()
+// we really should have middleware to check username and password usually. 
 
 router.post('/register', async (req, res, next) => {
 
@@ -24,7 +25,20 @@ router.post('/register', async (req, res, next) => {
 
 })
 router.post("/login", async (req, res, next) => {
-    res.json({ message: "login working" });
+   try{
+    const { username, password } = req.body;
+    const [user] = await User.findBy({username})
+    if(user &&  bcrypt.compareSync(password, user.password)) {
+       req.session.user = user //this is a very important line this signals the express session lib that a session needs to be saved for this user.  
+       res.json({message: `welcoem back, ${user.username}`})
+    } else {
+        next({status: 401, message: 'bad credentials '})
+    }
+
+
+   } catch (err) {
+    next(err)
+   }
 });
 
 
--- api/server.js
+++ api/server.js
@@ -18,6 +18,7 @@ server.use(session({
     secure: false,
     httpOnly: false,
   },
+  rolling: true,
   resave: false, 
   saveUninitialized: false, 
 }))
